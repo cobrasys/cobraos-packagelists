985b321672a672af256654343b3941127d254f67
RaidTheWeb <raidtheweb@tutanota.com>
2021-01-21 11:10:23 +1300

    Update g++.js

--- source/g++.js
+++ source/g++.js
@@ -4,56 +4,49 @@
 
 
 
-const Run = (context) => {
-    const { stdout, args } = context;
-
-    let code = '';
-    if(window.directory == '') {
-        // handle branches off of root
-        if(window.virtualDrive[''][args[0]]) {
-            if(window.virtualDrive[''][args[0]] instanceof(VirtualFile)) {
-                code = window.virtualDrive[''][args[0]].content
-            } else {
-                stdout.writeln('g++: ' + args[0] + ': is a directory.');
-                return;
-            }
+let code = '';
+if (window.directory == '') {
+    // handle branches off of root
+    if (window.virtualDrive[''][args[0]]) {
+        if (window.virtualDrive[''][args[0]] instanceof(VirtualFile)) {
+            code = window.virtualDrive[''][args[0]].content
         } else {
-            stdout.writeln('g++: ' + args[0] + ': no such file or directory');
+            stdout.writeln('g++: ' + args[0] + ': is a directory.');
             return;
         }
     } else {
-        // handle everything else
-        let workingdirectorysplit = window.directory.slice(1).split('/');
-                
-        let completestring = `window.virtualDrive['']`;
-        workingdirectorysplit.forEach(element => {
-           completestring += `['${element}']`;
-        });
+        stdout.writeln('g++: ' + args[0] + ': no such file or directory');
+        return;
+    }
+} else {
+    // handle everything else
+    let workingdirectorysplit = window.directory.slice(1).split('/');
+
+    let completestring = `window.virtualDrive['']`;
+    workingdirectorysplit.forEach(element => {
+        completestring += `['${element}']`;
+    });
 
 
-        if(eval(completestring + `['${args[0]}']`)) {
-            if(eval(completestring + `['${args[0]}']`) instanceof(VirtualFile)) {
-                code = eval(completestring + `['${args[0]}']`).content;
-            } else {
-                stdout.writeln('g++: ' + args[0] + ': is a directory.');
-                return;
-            }
+    if (eval(completestring + `['${args[0]}']`)) {
+        if (eval(completestring + `['${args[0]}']`) instanceof(VirtualFile)) {
+            code = eval(completestring + `['${args[0]}']`).content;
         } else {
-            stdout.writeln('g++: ' + args[0] + ': no such file or directory');
+            stdout.writeln('g++: ' + args[0] + ': is a directory.');
             return;
         }
+    } else {
+        stdout.writeln('g++: ' + args[0] + ': no such file or directory');
+        return;
     }
-    var input = "";
-    var config = {
-        stdio: {
-            write: function(s) {
-                term.write(s.replace('\n', '\r\n'));
-            }
-        },
-        unsigned_overflow: "error" // can be "error"(default), "warn" or "ignore"
-    };
-    var exitCode = window.JSCPP.run(code, input, config);
-	
 }
-
-window.Packages['g++'] = Run;
+var input = "";
+var config = {
+    stdio: {
+        write: function(s) {
+            term.write(s.replace('\n', '\r\n'));
+        }
+    },
+    unsigned_overflow: "error" // can be "error"(default), "warn" or "ignore"
+};
+var exitCode = window.JSCPP.run(code, input, config);
